c2df3c71b724081cc737bda6607c9ead6fc8dd06
AboudaSadok <aboudasadok@gmail.com>
2018-09-16 15:05:32 +0100

    machrou3 le3mor

---
+++ schoolrooom/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render() {
+    return (
+      <div>Abouda Sadok Zouari Mustapha</div>
+    );
+  }
+}
+
+export default App;
