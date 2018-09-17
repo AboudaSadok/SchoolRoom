abd3d23146e4e29089a8e1ff63d34932e9e18914
AboudaSadok <aboudasadok@gmail.com>
2018-09-17 06:28:10 +0100

    front show list

---
+++ schoolrooom/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import ROOT_URL from './consts';
+
+export const FETCH_PERSON = "fetch_person";
+/*export function fetchPerson() {
+  //const request = axios.get(`${ROOT_URL}/person/${id}`);
+
+  return {
+    type: FETCH_PERSON,
+    payload: request
+  };
+}
+*/
--- schoolrooom/src/components/App.js
+++ schoolrooom/src/components/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ROOT_URL from '../actions/consts';
+import Person from '../containers/person';
 
 class App extends Component{
+  state = {
+    persons:[]
+  };
+
+  setPersons = () => {
+    axios.get(`http://localhost:8082/persons`).then((data) =>
+      {
+        //console.log('qsfdqefqfeqfeqfeq',data.data);
+        var persons = data.data.map( (elt, index) =>
+          <Person
+            userName = { elt.userName }
+            fullName = { elt.fullName }
+            email = { elt.email }
+          />
+      );
+      this.setState ({
+        persons: persons
+      });
+      }
+  );
+  }
+
+
   render() {
+    this.setPersons();
     return (
-      <div>Abouda Sadok Zouari Mustapha</div>
+
+      <div>
+        {this.state.persons}
+      </div>
+
     );
   }
 }
